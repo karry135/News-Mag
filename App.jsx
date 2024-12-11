0d8e1665cbda47dfad23950e59ec12662c3d7eaf
karry135 <72527763+karry135@users.noreply.github.com>
2024-12-11 14:07:37 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,16 @@
+import Navbar from "./Componenets/Navbar"
+import NewsBoard from "./Componenets/NewsBoard"
+
+
+const App = () => {
+  return(
+
+    <div>
+     <Navbar/>
+     <NewsBoard/>
+    </div>
+  )
+
+}
+
+export default App
